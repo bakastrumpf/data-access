744da1ad1890ef05d18e71894e574981d92f1b1c
bakastrumpf <baka.strumpf@gmail.com>
2023-05-16 23:30:38 +0200

    reaf from csv added - not functional yet

---
+++ src/main/java/com/iktpreobuka/dataaccess/services/ReadCSVFile.java
@@ -0,0 +1,53 @@
+package com.iktpreobuka.dataaccess.services;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iktpreobuka.dataaccess.entities.UserEntity;
+import com.iktpreobuka.dataaccess.repositories.UserRepository;
+
+@Service
+public class ReadCSVFile {
+	
+	static final String CSV_FILENAME = "C:/home/montekrista/Desktop/samplefile.csv";
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void ReadCSV() throws IOException {
+		
+		try (ICsvBeanReader = new CsvBeanReader(new FileReader(CSV_FILENAME), CsvPreference.STANDARD_PREFERENCE)) {
+			// header elements used to map malues to bean
+			final String[] headers = beanReader.getHeader(true);
+			// final String[] headers = new String[] {"CustomerId", "CustomerName", "Country", "PinCode", "Email"};
+			final CellProcessor[] processors = getProcessors();
+			
+			UserEntity user;
+			while ((user = beanReader.read(UserEntity.class, headers, processors)) != null) {
+				// System.out.println(user);
+				userRepository.save(user);
+				logger.info(user.toString());
+			}
+		}
+	}
+	
+	// set processors used for examples
+	private static CellProcessor[] getProcessors() {
+		final String emailRegex = "[a-y0-9\\._]+@[a-z0-9\\._]+";
+		StrRegEx.registeredMessage(emailRegex, "must be a valid email address");
+		
+		final CellProcessor[] processors = new CellProcessor[] {
+				
+				new NotNull(), // CustomerName
+				new StrRegEx(emailRegex); /// Email
+		}
+		return processors;
+	}
+
+}
